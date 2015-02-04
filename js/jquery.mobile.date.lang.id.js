a26357cab8a36eac00414ab0e7d565af15f56e51
youngky andreas <seribusatu_it2@yahoo.com>
2015-02-04 15:50:56 +0700

    first push

---
+++ js/jquery.mobile.date.lang.id.js
@@ -0,0 +1,117 @@
+/*
+ * jQuery-Mobile-DateBox 
+ * Date: Wed Nov 19 2014 21:05:33 UTC
+ * http://dev.jtsage.com/jQM-DateBox/
+ * https://github.com/jtsage/jquery-mobile-datebox
+ *
+ * Copyright 2010, 2014 JTSage. and other contributors
+ * Released under the MIT license.
+ * https://github.com/jtsage/jquery-mobile-datebox/blob/master/LICENSE.txt
+ *
+ */
+jQuery.extend(jQuery.mobile.datebox.prototype.options.lang, { "id": {
+	"setDateButtonLabel": "Atur Tanggal",
+	"setTimeButtonLabel": "Atur Waktu",
+	"setDurationButtonLabel": "Atur Durasi",
+	"calTodayButtonLabel": "Lompat ke hari ini",
+	"titleDateDialogLabel": "Pilih Tanggal",
+	"titleTimeDialogLabel": "Pilih Waktu",
+	"daysOfWeek": [
+		"Minggu",
+		"Senin",
+		"Selasa",
+		"Rabu",
+		"Kamis",
+		"Jumat",
+		"Sabtu"
+	],
+	"daysOfWeekShort": [
+		"Mi",
+		"Se",
+		"Se",
+		"Ra",
+		"Ka",
+		"Jum",
+		"Sab"
+	],
+	"monthsOfYear": [
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember"
+	],
+	"monthsOfYearShort": [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"Me",
+		"Jun",
+		"Jul",
+		"Agu",
+		"Sep",
+		"Okt",
+		"Nov",
+		"Des"
+	],
+	"durationLabel": [
+		"Hari",
+		"Jam",
+		"Menit",
+		"Detik"
+	],
+	"durationDays": [
+		"Hari",
+		"Hari"
+	],
+	"tooltip": "Buka Date Picker",
+	"nextMonth": "Bulan Berikutnya",
+	"prevMonth": "Bulan Sebelumnya",
+	"timeFormat": 12,
+	"headerFormat": "%A, %B %-d, %Y",
+	"dateFieldOrder": [
+		"d",
+		"m",
+		"y"
+	],
+	"timeFieldOrder": [
+		"h",
+		"i"
+	],
+	"slideFieldOrder": [
+		"d",
+		"m",
+		"y"
+	],
+	"dateFormat": "%d/%m/%Y",
+	"useArabicIndic": false,
+	"isRTL": false,
+	"calStartDay": 1,
+	"clearButton": "Bersihkan",
+	"durationOrder": [
+		"d",
+		"h",
+		"i",
+		"s"
+	],
+	"meridiem": [
+		"AM",
+		"PM"
+	],
+	"timeOutput": "%l:%M %p",
+	"durationFormat": "%Dd %DA, %Dl:%DM:%DS",
+	"calDateListLabel": "Tanggal lainnya",
+	"calHeaderFormat": "%B %Y",
+	"calTomorrowButtonLabel": "Melompat ke besok"
+}});
+jQuery.extend(jQuery.mobile.datebox.prototype.options, {
+	useLang: "id"
+});
